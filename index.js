db4f9957bee1cfe8e83cf3c77c2e1013a624713c
martinmwas254 <martinmwas264@gmail.com>
2023-04-04 15:26:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+// return multiplied;
+console.log(multiply);
 
-// Write your code here
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 1345;
+let num4 = 4.5;
+
+let mod = num3 % num4;
+
+console.log(mod);
+
+let numbers = [10, 5, 20, 15];
+let max = Math.max(...numbers);
